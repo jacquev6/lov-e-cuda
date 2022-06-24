793f2dc50e57957921f08b6d5af7152ec3ba26d8
Vincent Jacques <vincent@vincent-jacques.net>
2022-06-24 09:14:24 +0200

    Fix naming

--- tests/array-views.cpp
+++ tests/array-views.cpp
@@ -10,7 +10,7 @@
 
 class ArrayViewTest : public testing::Test {
  protected:
-  ArrayViewTest() : m(s1, s0, memory) {
+  ArrayViewTest() : array(s1, s0, memory) {
     for (unsigned i1 = 0; i1 != s1; ++i1) {
       for (unsigned i0 = 0; i0 != s0; ++i0) {
         memory[i1 * s0 + i0] = 13 * i1 + 17 * i0;
@@ -21,54 +21,54 @@ class ArrayViewTest : public testing::Test {
   static const unsigned s1 = 4;
   static const unsigned s0 = 3;
   int memory[s1 * s0];  // NOLINT(runtime/arrays)
-  ArrayView2D<Host, int> m;
+  ArrayView2D<Host, int> array;
 };
 
 const unsigned ArrayViewTest::s1;
 const unsigned ArrayViewTest::s0;
 
 TEST_F(ArrayViewTest, GetSizes) {
-  EXPECT_EQ(m.s0(), s0);
-  EXPECT_EQ(m.s1(), s1);
+  EXPECT_EQ(array.s0(), s0);
+  EXPECT_EQ(array.s1(), s1);
 }
 
 TEST_F(ArrayViewTest, Index) {
-  // m[i1][i0] == memory[i1 * s0 + i0]
-  EXPECT_EQ(m[0][0], 0);
-  EXPECT_EQ(m[0][1], 17);
-  EXPECT_EQ(m[0][2], 34);
-  EXPECT_EQ(m[1][0], 13);
-  EXPECT_EQ(m[1][1], 30);
-  EXPECT_EQ(m[1][2], 47);
-  EXPECT_EQ(m[2][0], 26);
-  EXPECT_EQ(m[2][1], 43);
-  EXPECT_EQ(m[2][2], 60);
-  EXPECT_EQ(m[3][0], 39);
-  EXPECT_EQ(m[3][1], 56);
-  EXPECT_EQ(m[3][2], 73);
+  // array[i1][i0] == memory[i1 * s0 + i0]
+  EXPECT_EQ(array[0][0], 0);
+  EXPECT_EQ(array[0][1], 17);
+  EXPECT_EQ(array[0][2], 34);
+  EXPECT_EQ(array[1][0], 13);
+  EXPECT_EQ(array[1][1], 30);
+  EXPECT_EQ(array[1][2], 47);
+  EXPECT_EQ(array[2][0], 26);
+  EXPECT_EQ(array[2][1], 43);
+  EXPECT_EQ(array[2][2], 60);
+  EXPECT_EQ(array[3][0], 39);
+  EXPECT_EQ(array[3][1], 56);
+  EXPECT_EQ(array[3][2], 73);
 }
 
 TEST_F(ArrayViewTest, ConvertToConst) {
   // Can convert to const
-  ArrayView2D<Host, const int> cm(m);
+  ArrayView2D<Host, const int> const_array(array);
 
   // Can read from const
-  EXPECT_EQ(cm[2][1], 43);
+  EXPECT_EQ(const_array[2][1], 43);
 
   // Can't write to a const
   #if EXPECT_COMPILE_ERROR == __LINE__
-    cm[2][1] = 65;
+    const_array[2][1] = 65;
   #endif
 
   // Cant' convert back to non-const
   #if EXPECT_COMPILE_ERROR == __LINE__
-    ArrayView2D<Host, int> ncm(cm);
+    ArrayView2D<Host, int> non_const_array(const_array);
   #endif
 }
 
 TEST_F(ArrayViewTest, IndexOnce) {
-  ArrayView1D<Host, const int> m1 = m[1];
-  EXPECT_EQ(m1[0], 13);
-  EXPECT_EQ(m1[1], 30);
-  EXPECT_EQ(m1[2], 47);
+  ArrayView1D<Host, const int> array_1 = array[1];
+  EXPECT_EQ(array_1[0], 13);
+  EXPECT_EQ(array_1[1], 30);
+  EXPECT_EQ(array_1[2], 47);
 }
