b5a1847cbffe9ee1a8eb6f8a57c49c5689ccab65
Vincent Jacques <vincent@vincent-jacques.net>
2022-06-29 11:04:00 +0200

    Stop pretending we can do anything without an actual GPU

--- make.sh
+++ make.sh
@@ -17,21 +17,19 @@ fi
 
 if test -f build/nvidia-docker-runtime.ok || docker run --rm --gpus all nvidia/cuda:11.2.2-base-ubuntu20.04 nvidia-smi >/dev/null 2>&1
 then
-  docker_gpu_options="--gpus all --env LOV_E_HAS_GPU=true"
   touch build/nvidia-docker-runtime.ok
 else
   echo "************************************************************************"
   echo "** The NVidia Docker runtime does not seem to be properly configured. **"
-  echo "** Tests that require a GPU will *not* be run.                        **"
   echo "************************************************************************"
-  docker_gpu_options="--env LOV_E_HAS_GPU=false"
+  exit 1
 fi
 
 
 docker run --rm \
   --volume $PWD:/project --workdir /project \
   --user $(id -u):$(id -g) `# Avoid creating files as 'root'` \
-  $docker_gpu_options \
+  --gpus all \
   --network none `# Ensure the repository is self-contained (except for the "docker build" phase)` \
   --volume "$PWD:/wd" --workdir /wd \
   love-e-cuda-builder \
